97745b4e7f872fec3c6483444bc3f0faac451726
lsamoyedem <lucassamoyedem@gmail.com>
2022-05-09 01:25:00 -0300

    Alterações finais
    Alterações finais

--- agenda.py
+++ agenda.py
@@ -1,38 +1,44 @@
+from operator import contains
 import speech_recognition as sr
 import pyttsx3
 import datefinder as df
 import difflib as dif
+import re
 from googletrans import Translator
 
 
 recog = sr.Recognizer()
-
 translator = Translator()
 
+#Fala o texto passado e também retorna no console
 def SpeakText(text):
     out = pyttsx3.init()
     out.say(text)
     print(text)
     out.runAndWait()
 
+#Compara às duas string, se uma contém em outra, retorna True
 def ContainsStr(text, compare):
-    resposta = dif.get_close_matches(text, [compare])
-    return len(resposta) > 0 
+    return re.search(text, compare)
 
+#transforma o áudio em texto, fica ouvindo até o retorno ser diferente de nulo
 def Listening():
-    try:
-        with sr.Microphone() as mic:
-            recog.adjust_for_ambient_noise(mic, 1)          
-            print("Ouvindo...")
-            audio = recog.listen(mic)
-            return recog.recognize_google(audio, language='pt-BR')
-    except sr.RequestError as e:
-        print("Não foi possível requisitar o pedido: {0}".format(e))
-        Listening()
-    except: 
-        Listening()
+    while(True):
+        try:
+            with sr.Microphone() as mic:
+                recog.adjust_for_ambient_noise(mic, 1)          
+                print("Ouvindo...")
+                audio = recog.listen(mic)
+                text = recog.recognize_google(audio, language='pt-BR')
+                if text:
+                    return text 
+        except sr.RequestError as e:
+            print("Não foi possível requisitar o pedido: {0}".format(e))
+        except:
+            print("Erro") 
 
-def confirma():
+#Espera uma confirmação do usuário através de voz, se 'sim', retorna True, se 'não' retorna False 
+def Confirm():
     while(True):
         resposta = Listening()
         print(resposta)
@@ -43,27 +49,30 @@ def confirma():
             SpeakText("Ok!")
             return False
         else:
-            SpeakText("Não entendi! Diga Sim para confirmar ou não para tentar novamente")
+            SpeakText("Não entendi! Diga 'sim' para confirmar ou 'não' para tentar novamente")
 
-def findDate(text):
+#Procura uma data na string, se contém retorna a primeira encontrada. Formatada em dd/mm/yyyy
+def FindDate(text):
     translateEn = translator.translate(text, dest= 'en', src ='pt')
     matches = df.find_dates(translateEn.text)
     for match in matches:
         return match.strftime("%d/%m/%Y")
 
+#Espera uma data através de um comando de voz
 def ReadDate():
     while(True):
         try:
             SpeakText("Diga a data do evento:") 
             dataText = Listening()
             print(dataText)
-            dataFormat = findDate(dataText)
+            dataFormat = FindDate(dataText)
             SpeakText("Você disse: " + dataFormat + "?")
-            if confirma():
+            if Confirm():
                 return dataFormat 
         except:
             SpeakText("Não foi possível identificar a data")
 
+#Espera uma descrição
 def ReadDescription():
     while(True):
         try:
@@ -74,47 +83,81 @@ def ReadDescription():
             SpeakText("Não entendi")
 
 
-def CriarEvento(date):
+#Cria um evento, salvando em um arquivo .txt
+def CreateEvent(date):
     if date is None:
         SpeakText("Ok, vamos criar um novo evento")
         date = ReadDate()
     else:
-        SpeakText("Deseja  criar um evento  na data " + date + "?")
-        if not confirma():
+        SpeakText("Deseja criar um evento na data " + date + "?")
+        if not Confirm():
             date = ReadDate()
     desc = ReadDescription()
     SpeakText("Confirma a criação do evento " + desc + " na data " + date + "?")
-    if confirma():
-        SpeakText("Ok! evento salvo")
+    if Confirm():
+        SpeakText("Evento salvo!")
         f = open("dados.txt", "a")
         f.write(date + ";" + desc + "\n")
         f.close()
 
-def ReadEvents(date):
+#Lista os eventos através da voz, tanto por data quanto pela descrição
+def ReadEvents():
+    SpeakText("Diga a data ou descrição do evento:")
+    text = Listening()
+    date = FindDate(text)
     if date is None:
-        SpeakText("Ok, vamos criar um novo evento")
-        date = ReadDate()
-    with open('dados.txt') as f:
-        lista = []
-        for line in f.readlines():
-            dateLine = line.split(";")[0]
-            if dateLine == date:
-                lista.append(line.split(";")[1])
-        SpeakText("Os eventos da data " + date + "são")
-        for desc in lista:
-            SpeakText(desc)
+        with open('dados.txt') as f:
+            lista = []
+            for line in f.readlines():
+                desc = line.split(";")[1]
+                desc = desc.replace("\n", "")
+                if ContainsStr(text, desc):
+                    lista.append(line.split(";")[0] + ": " + line.split(";")[1])
+            if len(lista) > 0:     
+                SpeakText("Encontrei as seguintes datas para a descrição " + text + ":")
+                for data in lista:
+                    SpeakText(data)
+            else:
+                SpeakText("Não encontrei nenhum evento com a descrição " + text)
+    else:
+        with open('dados.txt') as f:
+            lista = []
+            for line in f.readlines():
+                dateLine = line.split(";")[0]
+                if dateLine == date:
+                   lista.append(line.split(";")[0] + ": " + line.split(";")[1])
+            if len(lista) > 0:
+                SpeakText("Encontrei os seguintes eventos para a data " + date + ":")
+                for desc in lista:
+                    SpeakText(desc)
+            else:
+                SpeakText("Não encontrei nenhum evento na data " + date )
 
+#Recebe os comandos a serem executados
 def Command(command):
-    if ContainsStr("desligar", command):
+    command = command.lower()
+    resposta = dif.get_close_matches(command.lower(), ["criar evento", "listar evento", "desligar", "ajuda"])
+    compare = ""
+    if len(resposta) > 0:
+        compare = resposta[0]
+
+    if compare == "desligar":
        SpeakText("Ok! Estou desligando...")
-    elif ContainsStr("Criar evento", command):
-        date = findDate(command) 
-        CriarEvento(date)
-    elif ContainsStr("listar eventos", command):
-        date = findDate(command) 
-        ReadEvents(date)
+    elif compare == "criar evento":
+        date = FindDate(command) 
+        CreateEvent(date)
+    elif compare == "listar evento":
+        ReadEvents()
+    elif compare == "ajuda":
+         SpeakText("Você pode dizer os seguintes comandos:\n"
+                   + " Criar Evento, para criar um evento em uma data específica;\n"
+                   + " Listar Evento, para listar os eventos de uma data, ou listar as datas através de uma descrição;\n"
+                   + " Desligar, para encerar o programa;")
     else:
         SpeakText("Não entendi (" + command +")")
+
+#Início da execução, espera o comando do usuário através da voz 
+SpeakText("Olá, Bem vindo")
 
 text = ""
 while(text != "desligar"):
